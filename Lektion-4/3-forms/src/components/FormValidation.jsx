8624cca314942f64e53190939678cc9510d4869d
Joakim Lindh <joakim.wahlstrom@epndata.com>
2024-01-11 14:45:59 +0100

    lektion 4

--- Lektion-4/3-forms/src/components/FormValidation.jsx
+++ Lektion-4/3-forms/src/components/FormValidation.jsx
@@ -12,7 +12,7 @@ const baseState = {
 export const FormValidation = () => {
   
   const [formData, setFormData] = useState(baseState)
-  const [formErrors, setFormErrors] = useState(baseState)
+  const [formErrors, setFormErrors] = useState({})
 
   const handleChange = e => {
     setFormData(data => {
@@ -26,6 +26,54 @@ export const FormValidation = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
+
+    if(!validate()) return
+
+    console.log('skicka till databasen')
+  }
+
+  const validate = () => {
+    const err = {}
+
+    if(formData.firstName.trim() === '') {
+      err.firstName = 'You need to enter a first name'
+    } else if (formData.firstName.trim().length < 2) {
+      err.firstName = 'Your name must be atleast 2 chars long'
+    }
+
+    if(formData.lastName.trim() === '') {
+      err.lastName = 'You need to enter a last name'
+    } else if (formData.lastName.trim().length < 2) {
+      err.lastName = 'Your name must be atleast 2 chars long'
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    if(formData.email.trim() === '') {
+      err.email = 'You need to enter an email address'
+    } else if(!emailRegex.test(formData.email)) {
+      err.email = 'Email not valid!'
+    }
+
+
+    // console.log(err)
+    // console.log(Object.keys(err).length)
+    // const test = Object.keys(err).length < 1
+    // console.log(test)
+
+    setFormErrors(err)
+
+    const errorsArray = Object.keys(err)
+    const lengthOfArray = errorsArray.length
+    let returnValue
+    if(lengthOfArray < 1) {
+      returnValue = true
+    } else {
+      returnValue = false
+    }
+
+    // return Object.keys(err).length < 1
+    return returnValue
   }
 
   return (
